a09306bd0fa6b49f873606b859a9ea04dd7758da
Linox <macove.polo@gmail.com>
2023-07-11 19:13:26 -0500

    validacion corregida

--- assets/use-tree-walker-235df7f4.js
+++
@@ -1,1 +0,0 @@
-import{m as f}from"./use-outside-click-8e8f26b4.js";import{w as d}from"./index-cfa66dfb.js";function m({container:e,accept:r,walk:n,enabled:t}){d(()=>{let a=e.value;if(!a||t!==void 0&&!t.value)return;let c=f(e);if(!c)return;let l=Object.assign(i=>r(i),{acceptNode:r}),o=c.createTreeWalker(a,NodeFilter.SHOW_ELEMENT,l,!1);for(;o.nextNode();)n(o.currentNode)})}export{m as p};
